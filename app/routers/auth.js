9fcbc42530b50961971726cf29edfc3dcf8aa401
TennnyZhuang <zty0826@gmail.com>
2016-11-25 17:55:52 +0800

    register API

--- app/routers/auth.js
+++ app/routers/auth.js
@@ -1,11 +1,30 @@
 const Router = require('koa-router')
+const AuthUtil = require('../thulib/auth').AuthUtil
 
 const router = new Router({
-  prefix: '/auth'
+  prefix: '/students'
 })
 
-router.get('/', async (ctx) => {
-  ctx.body = 'Hello World'
+router.post('/register', async ctx => {
+  const {username, password} = ctx.request.fields || {}
+  if (!username || !password) {
+    ctx.throw(400, 'Missing Arguments')
+  }
+
+  const result = await AuthUtil.auth(username, password)
+
+  ctx.body = {
+    username: username
+  }
+
+  if (result) {
+    ctx.body.message = 'Success'
+    ctx.body.information = await AuthUtil.getUserInfo(username, password)
+
+    // TODO: save user
+  } else {
+    ctx.body.message = 'Failure'
+  }
 })
 
 exports.router = router
