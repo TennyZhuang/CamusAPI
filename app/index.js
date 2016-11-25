4182fcce9a2975c4c5190e28c08aff7b9b2547ee
TennnyZhuang <zty0826@gmail.com>
2016-11-25 18:22:24 +0800

    middleware lowercase

--- app/index.js
+++ app/index.js
@@ -3,12 +3,14 @@ const logger = require('koa-logger')
 const restc = require('restc')
 const bodyParser = require('koa-better-body')
 const routers = require('./routers/base')
+const lowercase = require('./middlewares/lowercase').lowercase
 
 const app = new Koa()
 
 app.use(logger())
 app.use(bodyParser())
 app.use(restc.koa2())
+app.use(lowercase)
 
 routers.initApp(app)
 
---
+++ app/middlewares/lowercase.js
@@ -0,0 +1,18 @@
+const _lowercase = (rawObj) => {
+  const obj = {}
+
+  for (let [k, v] of Object.entries(rawObj)) {
+    obj[k.toLowerCase()] = (typeof v === 'object' ? _lowercase(v) : v)
+  }
+
+  return obj
+}
+
+const lowercase = async (ctx, next) => {
+  await next()
+  if (typeof ctx.body === 'object') {
+    ctx.body = _lowercase(ctx.body)
+  }
+}
+
+exports.lowercase = lowercase
