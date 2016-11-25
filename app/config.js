99254bcf99f9b1ddc6a10034c8016c861131b089
TennnyZhuang <zty0826@gmail.com>
2016-11-25 13:57:19 +0800

    config file

---
+++ app/config.js
@@ -0,0 +1,29 @@
+const process = require('process')
+
+class Config {
+  constructor () {
+    this.debug = true
+    this.production = false
+  }
+}
+
+class DevelopmentConfig extends Config {
+  constructor () {
+    super()
+  }
+}
+
+class ProductionConfig extends Config {
+  constructor () {
+    super()
+    this.debug = false
+    this.production = true
+  }
+}
+
+configs = new Map([
+  ['development', new DevelopmentConfig()],
+  ['production', new ProductionConfig()]
+])
+
+exports.config = configs.get(process.env.CAMUS_API_ENV || 'development')
