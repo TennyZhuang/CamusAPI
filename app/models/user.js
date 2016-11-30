22d487fe2c34bb02938a3c7669d0292e468148e8
TennnyZhuang <zty0826@gmail.com>
2016-11-30 11:42:57 +0800

    models: user

---
+++ app/models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const POSITIONS = ['undergraduate', 'master', 'doctor', 'teacher', 'unknown']
+
+const userInfoSchema = new mongoose.Schema({
+  studentNumber: String,
+  department: String,
+  realname: String,
+  position: {
+    type: String,
+    enum: POSITIONS
+  }
+}, {
+  toObject: {
+    transform: (doc, ret) => {
+      delete ret._id
+    }
+  }
+})
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    index: true
+  },
+  password: String,
+  info: userInfoSchema
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
