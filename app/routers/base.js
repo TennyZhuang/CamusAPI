484850575ab79fce4420875c3967f9384d667a11
TennnyZhuang <zty0826@gmail.com>
2016-12-01 00:25:34 +0800

    library router

--- app/routers/base.js
+++ app/routers/base.js
@@ -1,7 +1,9 @@
 const auth = require('./auth')
+const library = require('./library')
 
 const initApp = (app) => {
   app.use(auth.router.routes()).use(auth.router.allowedMethods())
+  app.use(library.router.routes()).use(library.router.allowedMethods())
 }
 
 exports.initApp = initApp
