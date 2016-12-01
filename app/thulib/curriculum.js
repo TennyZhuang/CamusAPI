ebedefa72a2d6323b3b1bd925ec0251eccdae11e
yexithu <yexithu14@163.com>
2016-12-01 18:52:04 +0800

    Parse weekStr in curriculum

--- app/thulib/curriculum.js
+++ app/thulib/curriculum.js
@@ -1,10 +1,6 @@
 /**
  * Created by XiYe on 12/1/2016.
  */
-
-/**
- * Created by XiYe on 12/1/2016.
- */
 const rp = require('request-promise').defaults({jar: true})
 const ci = require('cheerio')
 const iconv = require('iconv-lite')
@@ -34,17 +30,77 @@ class CurriculumUtil {
         return ci.load(html, {decodeEntities: false})
       }
     }
-    const parseFirstLevel =($) => {
-      const coursesDict = {}
+
+    const parseWeekStr = (s) => {
+      const range = (start, end, stride = 1) => {
+        if (isNaN(start) || isNaN(end)) {
+          throw "Unknown Curriculum Week Format Exception"
+        }
+        let x = []
+        for (let i = start; i <= end; i += stride) {
+          x.push(i)
+        }
+        return x
+      }
+      let week = null
+      switch (s) {
+        case "全周":
+          week = range(1, 16)
+          break
+        case "前八周":
+          week = range(1, 8)
+          break
+        case "后八周":
+          week = range(9, 16)
+          break
+        case "单周":
+          week = range(1, 16, 2)
+          break
+        case "双周":
+          week = range(2, 16, 2)
+          break
+        default:
+          let index = s.indexOf('-')
+          while (index != -1) {
+            week = week.concat(range(parseInt(s[index - 1]), parseInt(s[index + 1])))
+            s = s.slice(index + 2, s.length)
+            index = s.indexOf('-')
+          }
+          break
+      }
+      return week
+    }
+
+    const parseFirstLevelCurriculum =($) => {
       $('.kc_div').each((i, elem) => {
         const course = {}
-        course['courseid'] = 'id'
-        course['coursename'] = $(elem).find('h5').first().children().first().text()
-        course['time'] = '[[1-7,1-6]]'
-        course['teacher'] = 'teacher'
-        course['classroom'] = 'classroom'
-        course['weeks'] = [1]
-        courses[i] = course
+
+        //Course ID and Time
+        let classStr = $(elem).attr('class').split(" ")[1]
+        let courseID = classStr.slice(2, classStr.length - 1)
+        let time = [parseInt(classStr[0]), parseInt(classStr[1])]
+        //Course Name
+        let courseName = $(elem).find('h5').first().children().first().text()
+        //Teacher
+        let teacherStr = $(elem).find('h5').first().children().last().text()
+        let teacher = teacherStr.slice(0, teacherStr.indexOf("&"))
+
+        let li = $(elem).find('li').get(1)
+        //Weeks
+        let weekStr = $(li).find('span').first().text()
+        weekStr = weekStr.slice(weekStr.indexOf('(') + 1, weekStr.indexOf(')'))
+        let week = parseWeekStr(weekStr)
+
+        //Classroom
+        let classroom = $(li).find('span').last().text()
+
+        course['coursename'] = courseName
+        course['teacher'] = teacher
+        course['courseid'] = courseID
+        course['time'] = time
+        course['classroom'] = classroom
+        course['week'] = week
+        console.log(course)
       })
     }
     const crawlCurriculum = () => {
