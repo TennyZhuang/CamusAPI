7c8fb6f19e5a959fd859d9c3456657d32c2aad70
mzj14 <mzj14@mails.tsinghua.edu.cn>
2016-12-03 19:33:55 +0800

    enrich the auth test, consider multiple cases

--- test/auth/test.js
+++ test/auth/test.js
@@ -1,28 +1,82 @@
 /**
  * Created by Ma_Zi_jun on 2016/12/3.
  */
+
+/* Test summary for this function:
+ 1. With incomplete post data, the response show miss arguments
+ 2.1 With valid (username, password) for the first time, the response show success and existed = true
+ 2.2 With valid (username, password) for the second time, the response show success and existed = false
+ 3. With valid username and invalid password, the response show failure
+ 4. With invalid (username, password), the response show failure
+ */
+
 const nock = require('nock')
 
-const testAuthFeature = (agent) => {
+const testAuthFeature = (agent, validUsername, validPassword) => {
   describe('Test For Authentication Feature', () => {
-    describe('With incomplete data', () => {
+    describe('1. With incomplete data', () => {
       it('should response 400 with missing arguments', (done) => {
         agent
           .post('/users/register')
-          .send({username: 'user'})
+          .send({username: 'xxx'})
           .expect('Content-Type', /text/)
           .expect(400)
           .expect(/Missing Arguments/, done)
       })
     })
 
-    describe('With invalid data', () => {
+    describe('2. With valid (username, password) for twice', () => {
+      const validSubKeys = ['studentnumber', 'realname', 'position', 'department', 'email']
+      const positions = ['undergraduate', 'master', 'doctor', 'teacher']
+
+      //TODO: This needs mock for outer service but I can't figure out the way
+      it('2.1 should response 200 with message Success but existed false', (done) => {
+        agent
+          .post('/users/register')
+          .send({username: validUsername, password: validPassword})
+          .expect(200)
+          .expect('Content-Type', /json/)
+          .expect((res) => {
+            res.body.should.containEql({message: 'Success', username: validUsername, existed: false})
+            res.body.should.have.property('information')
+            const info = res.body.information
+            info.should.have.properties(validSubKeys)
+            info.position.should.be.equalOneOf(positions)
+          })
+          .end(done)
+      })
+
+      it('2.2 should response 200 with message Success but existed true', (done) => {
+        agent
+          .post('/users/register')
+          .send({username: validUsername, password: validPassword})
+          .expect(200)
+          .expect((res) => {
+            res.body.should.have.property('existed', true)
+          })
+          .end(done)
+      })
+    })
+
+    describe('3. With valid username and but invalid password', () => {
+      it('should response 400 with message Failure', (done) => {
+        //TODO: The test can't be passed currently. Correct the app code.
+        agent
+          .post('/users/register')
+          .send({username: validUsername, password: 'xxx'})
+          .expect(400)
+          .expect('Content-Type', /json/)
+          .expect({message: 'Failure', username: 'xxx'}, done)
+      })
+    })
+
+    describe('4. With invalid (username, password) ', () => {
       it('should response 400 with message Failure', (done) => {
         const outerDomain = 'https://id.tsinghua.edu.cn'
         const appID = 'ALL_ZHJW'
         const outerPath = `/thuser/authapi/login/${appID}/0_0_0_0`
         const response = {
-          authCode: 261,
+          authCode: 266,
           status: 'RESTLOGIN_ERROR_AUTH'
         }
 
@@ -32,39 +86,12 @@ const testAuthFeature = (agent) => {
 
         agent
           .post('/users/register')
-          .send({username: 'user', password: 'invalid'})
+          .send({username: 'xxx', password: 'xxx'})
           .expect(400)
           .expect('Content-Type', /json/)
-          .expect({message: 'Failure', username: 'user'}, done)
+          .expect({message: 'Failure', username: 'xxx'}, done)
       })
     })
-
-    describe('With valid data', () => {
-      // we cannot mock the right ticket of the outer service
-      // TODO: provide a valid username and password to pass this describe
-      const validUserName = 'a valid user name'
-      const validPassword = 'a valid password'
-      const validSubKeys = ['studentnumber', 'realname', 'position', 'department', 'email']
-      const positions = ['undergraduate', 'master', 'doctor', 'teacher']
-
-      it('should response 200 with message Success', (done) => {
-        agent
-          .post('/users/register')
-          .send({username: validUserName, password: validPassword})
-          .expect(200)
-          .expect('Content-Type', /json/)
-          .expect((res) => {
-            res.body.should.containEql({message: 'Success', username: validUserName})
-            res.body.should.have.properties(['existed', 'information'])
-            res.body.existed.should.be.a.Boolean()
-            const info = res.body.information
-            info.should.have.properties(validSubKeys)
-            info.position.should.be.equalOneOf(positions)
-          })
-          .end(done)
-      })
-    })
-    
   })
 }
 
