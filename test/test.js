a37ff9d699497051f6d9ccba868d64d0e0331e3c
mzj14 <mzj14@mails.tsinghua.edu.cn>
2016-12-03 19:35:46 +0800

    add  canceltest module to the test

--- test/test.js
+++ test/test.js
@@ -2,12 +2,21 @@
  * Created by Ma_Zi_jun on 2016/12/1.
  */
 const request = require('supertest')
+
 const app = require('../app/index')
+
 const testAuthFeature = require('./auth/test')
 const testLibraryFeature = require('./library-hs/test')
+const testCancelFeature = require('./cancel/test')
 
 const agent = request.agent(app.listen())
 
-testAuthFeature(agent)
+//TODO: Provide validUsername and validPassword to pass all the test
+const validUsername = 'username'
+const validPassword = 'password'
+
+testAuthFeature(agent, validUsername, validPassword)
 
 testLibraryFeature(agent)
+
+testCancelFeature(agent, validUsername)
