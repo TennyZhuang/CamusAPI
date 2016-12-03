17259034b7ec85adb691876ec09371659b83fb0c
mzj14 <mzj14@mails.tsinghua.edu.cn>
2016-12-03 21:16:04 +0800

    add log to mogo connection

--- app/models/base.js
+++ app/models/base.js
@@ -5,6 +5,7 @@ mongoose.Promise = global.Promise
 
 const connect = () => {
   mongoose.connect(config.mongodbUrl)
+  console.log('Connecting to mongodb at:', config.mongodbUrl)
 }
 
 exports.connect = connect
