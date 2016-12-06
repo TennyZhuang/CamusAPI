d6e31d5dd9f4ebc3c8ba3c56e4759b8382dd1cb5
Songzhou Yang <yangsongzhou@outlook.com>
2016-12-06 16:07:26 +0800

    add query of courseNS

--- app/tasks/get_course_info.js
+++ app/tasks/get_course_info.js
@@ -2,8 +2,8 @@
  * Created by Songzhou Yang on 12/1/2016.
  */
 
-const getCourse = async (user, courseID) => {
-  return user.courses.find(c => c.courseID === courseID)
+const getCourse = async(user, courseID) => {
+  return user.courses.find(c => c.courseID === courseID || c.courseNS === courseID)
 }
 
 exports.getCourse = getCourse
