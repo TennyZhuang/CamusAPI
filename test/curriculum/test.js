1e6a6b4ddaa8aa3a42949a9ec0df1ad0790d772c
mzj14 <mzj14@mails.tsinghua.edu.cn>
2016-12-08 09:59:20 +0800

    add more test to curriculum class

--- test/curriculum/test.js
+++ test/curriculum/test.js
@@ -4,6 +4,27 @@
 const cur = require('../../app/thulib/curriculum')
 const readFile = require('fs-readfile-promise')
 const cheerio = require('cheerio')
+const nock = require('nock')
+const iconv = require('iconv-lite')
+
+const assertClasses = (classes) => {
+  classes.should.be.Array().and.should.not.be.empty()
+
+  const lesson = classes[0]
+  const properties = ['courseid', 'coursename', 'time', 'teacher', 'classroom', 'week']
+  lesson.should.have.properties(properties)
+
+  const time = lesson.time
+  time.should.be.Array().and.have.length(2)
+  time[0].should.be.Number().and.within(1, 7)
+  time[1].should.be.Number().and.within(1, 6)
+
+  const week = lesson.week
+  week.should.be.Array().and.have.length(16)
+  week.forEach((ele) => {
+    ele.should.be.Number().and.within(0, 1)
+  })
+}
 
 describe('1. test week parser', () => {
   it('1.1 should return right week array based on different key word', () => {
@@ -42,22 +63,7 @@ describe('2. test curriculum parser', () => {
     const data = await readFile(`${__dirname}\\test-right.html`, 'utf8')
     const $ = cheerio.load(data, { decodeEntities: false })
     const classes = await cur.parseFirstLevelCurriculum($)
-    classes.should.be.Array().and.should.not.be.empty()
-    
-    const lesson = classes[0]
-    const properties = ['courseid', 'coursename', 'time', 'teacher', 'classroom', 'week']
-    lesson.should.have.properties(properties)
-
-    const time = lesson.time
-    time.should.be.Array().and.have.length(2)
-    time[0].should.be.Number().and.within(1, 7)
-    time[1].should.be.Number().and.within(1, 6)
-
-    const week = lesson.week
-    week.should.be.Array().and.have.length(16)
-    week.forEach((ele) => {
-      ele.should.be.Number().and.within(0, 1)
-    })
+    assertClasses(classes)
   })
   
   //FIXME: How to test an exception thrown by async function properly
@@ -76,4 +82,33 @@ describe('2. test curriculum parser', () => {
 })
 
 
+describe('3. test curriculum getter', function () {
+  this.timeout(0)
+  it('3.1 curriculum info should be properly extracted with right username and password', async () => {
+    const buffer = await readFile(`${__dirname}\\test-right.html`)
+    // console.log(buffer)
+    const response = iconv.encode(buffer, 'GBK')
+    const outerDomain = 'http://zhjw.cic.tsinghua.edu.cn'
+    
+    nock(outerDomain)
+      .get('/j_acegi_login.do')
+      .query((query) => {
+        return ('ticket' in query)
+      })
+      .reply(200, {})
+
+    // console.log(response)
+
+    nock(outerDomain)
+      .get('/portal3rd.do')
+      .query((query) => {
+        return ('m' in query && 'mobile' in query)
+      })
+      .reply(200, response)
+
+    // let classes = []
+    const classes = await cur.getFirstLevelCurriculum('mzj14', 'a12345k8360', true)
+    assertClasses(classes)
+  })
+})
 
