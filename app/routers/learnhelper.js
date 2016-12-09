f77b9a1d35d43a0dbf4dea6bbe7a6fb9aea38194
mzj14 <mzj14@mails.tsinghua.edu.cn>
2016-12-09 17:25:45 +0800

    add checkCourse middleware to learnhelper router

--- app/routers/learnhelper.js
+++ app/routers/learnhelper.js
@@ -3,46 +3,39 @@
  */
 
 const Router = require('koa-router')
-const courseInfoGetter = require('../tasks/get_course_info')
-const getCourse = courseInfoGetter.getCourse
 const checkUser = require('../middlewares/checkuser')
+const checkCourse = require('../middlewares/checkcourse')
 
 const router = new Router({
   prefix: '/learnhelper'
 })
+
+router.param('courseID', checkCourse)
 
 router.param('username', checkUser)
 
 router.post('/:username/courses', async(ctx) => {
   const user = ctx.user
 
-  ctx.body = {
-    courses: user.courses.toObject()
-  }
+  ctx.body.courses = user.courses.toObject()
 })
 
 router.post('/:username/courses/:courseID/notices', async(ctx) => {
-  const {courseID} = ctx.params
-  const course = await getCourse(ctx.user, courseID)
-  ctx.body = {
-    notices: course.notices.toObject()
-  }
+  const course = ctx.course
+  
+  ctx.body.notices = course.notices.toObject()
 })
 
 router.post('/:username/courses/:courseID/documents', async(ctx) => {
-  const {courseID} = ctx.params
-  const course = await getCourse(ctx.user, courseID)
-  ctx.body = {
-    documents: course.documents.toObject()
-  }
+  const course = ctx.course
+  
+  ctx.body.documents = course.documents.toObject()
 })
 
 router.post('/:username/courses/:courseID/assignments', async(ctx) => {
-  const {courseID} = ctx.params
-  const course = await getCourse(ctx.user, courseID)
-  ctx.body = {
-    assignments: course.assignments.toObject()
-  }
+  const course = ctx.course
+  
+  ctx.body.assignments = course.assignments.toObject()
 })
 
 exports.router = router
