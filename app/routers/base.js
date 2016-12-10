62694a18331227bbd44247ca93fb2965b5f38a86
TennnyZhuang <zty0826@gmail.com>
2016-12-10 12:09:16 +0800

    rename auth to users

--- app/routers/base.js
+++ app/routers/base.js
@@ -1,11 +1,11 @@
-const auth = require('./auth')
+const users = require('./users')
 const library = require('./library')
 const learnhelper = require('./learnhelper')
 const curriculum = require('./curriculum')
 const event = require('./event')
 
 const initApp = (app) => {
-  app.use(auth.router.routes()).use(auth.router.allowedMethods())
+  app.use(users.router.routes()).use(users.router.allowedMethods())
   app.use(library.router.routes()).use(library.router.allowedMethods())
   app.use(learnhelper.router.routes()).use(learnhelper.router.allowedMethods())
   app.use(curriculum.router.routes()).use(curriculum.router.allowedMethods())
