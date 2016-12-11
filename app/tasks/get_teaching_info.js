692446fae56c0a99db26a3996b20c2f1c3b674a2
Songzhou Yang <yangsongzhou@outlook.com>
2016-12-11 16:47:28 +0800

    modify return msg

--- app/tasks/get_teaching_info.js
+++ app/tasks/get_teaching_info.js
@@ -7,7 +7,7 @@ const TeachingInfo = require('../models/teaching_info')
 const getTeachingInfo = async() => {
   const _info = await TeachingInfo.findOne({})
   const info = {}
-  info.time = _info.time
+  info.updateTime = _info.time
   info.currentSemester = _info.currentSemester.toObject()
   info.currentTeachingWeek = _info.currentTeachingWeek.toObject()
   info.nextSemester = _info.nextSemester.toObject()
