bcb9f08e68d230e0f8fe546314e028193afb1a55
mzj14 <mzj14@mails.tsinghua.edu.cn>
2016-12-27 19:10:10 +0800

    update the test for libray

--- test/library-hs/test.js
+++ test/library-hs/test.js
@@ -1,47 +1,33 @@
+const lib = require('../../app/thulib/library')
 const nock = require('nock')
 const readFile = require('fs-readfile-promise')
 
-const testLibraryFeature = (agent) => {
-  describe('Test For Library Feature', () => {
-    describe('Should return libray seats info', () => {
-      it('should response 200 with message success', (done) => {
-        const nockWithHtmlResponse = async function (outerDomain, outerPath, fileName) {
-          const htmlResponse = await readFile(fileName)
+describe('Test For Library Utility', () => {
+  describe('1. test method "fetch"', function () {
+    it('1.1 should response 200 with seat info', async () => {
+      const outerDomain = 'http://seat.lib.tsinghua.edu.cn'
+      const outerPath = '/roomshow/'
+      const fileName = 'test.html'
+      const htmlResponse = await readFile(`${__dirname}\\${fileName}`)
 
-          nock(outerDomain)
-            .get(outerPath)
-            .reply(200, htmlResponse)
-        }
-
-        before(() => {
-          const outerDomain = 'http://seat.lib.tsinghua.edu.cn/'
-          const outerPath = '/roomshow'
-          const fileName = 'test.html'
-
-          nockWithHtmlResponse(outerDomain, outerPath, fileName)
+      nock(outerDomain)
+        .get(outerPath)
+        .query((query) => {
+          return (query == null)
         })
-
-        agent
-          .post('/library/hs')
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .expect((res) => {
-            res.body.should.have.property('message', 'Success')
-            res.body.should.have.property('areas')
-            res.body.areas.should.be.Array().and.have.lengthOf(8)
-
-            const area = res.body.areas[0]
-
-            area.should.have.properties(['name', 'left', 'used'])
-            area.left.should.be.a.Number()
-            area.left.should.be.aboveOrEqual(0)
-            area.used.should.be.a.Number()
-            area.used.should.be.aboveOrEqual(0)
-          })
-          .end(done)
-      })
+        .reply(200, htmlResponse)
+      
+      await lib.fetch()
+      const areas = lib.result
+      
+      areas.should.be.Array().and.have.lengthOf(8)
+      
+      const area = areas[0]
+      area.should.have.properties(['name', 'left', 'used'])
+      area.left.should.be.a.Number()
+      area.left.should.be.aboveOrEqual(0)
+      area.used.should.be.a.Number()
+      area.used.should.be.aboveOrEqual(0)
     })
   })
-}
-
-module.exports = testLibraryFeature
+})
