6826feb445534b73c50ce8e50640f5216dd4211e
mzj14 <mzj14@mails.tsinghua.edu.cn>
2016-12-28 22:21:39 +0800

    finish the test for get assignments

--- test/learnhelper/test.js
+++ test/learnhelper/test.js
@@ -1,8 +1,8 @@
 /* Test flow
  Test 3 class functions
- 1. Mock html reply to test the `getCourseList` function
- 2. Use right html page and wrong html page to test the `parseFirstLevelCurriculum` function
- 3. Mock html reply test the `getFirstLevelCurriculum` function
+ 1. Mock html reply to test `getCourseList` function
+ 2. Mock html reply to test `getDocuments` function
+ 3. Mock html reply to test `getAssignments` function
  */
 const LearnHelperUtil = require('../../app/thulib/learnhelper')
 const learnHelper = new LearnHelperUtil('xxx', 'xxxx')
@@ -39,15 +39,16 @@ const assertDocs = (docs) => {
 
   doc.updatingTime.should.be.Number().and.aboveOrEqual(0)
 
-  doc.state.should.match(/已经提交|尚未提交/)
+  doc.state.should.match(/new|previous|unknown/)
 }
 
 const assertAssignments = (assignments) => {
   assignments.should.be.Array().and.should.not.be.empty()
 
   const assignment = assignments[0]
-  const properties = ['title', 'startDate', 'dueDate', 'state', 'size', 'assignmentID', 'detail', 'filename', 'fileURL', 'evaluatingTeacher',
-    'evaluatingDate', 'scored', 'grade', 'comment']
+  
+  const properties = ['title', 'startDate', 'dueDate', 'state', 'size', 'assignmentID', 'detail', 'filename', 'fileURL', 
+    'evaluatingTeacher', 'evaluatingDate', 'scored', 'grade', 'comment']
 
   assignment.should.have.properties(properties)
 
@@ -55,11 +56,11 @@ const assertAssignments = (assignments) => {
   assignment.dueDate.should.be.Number().and.aboveOrEqual(0)
   assignment.evaluatingDate.should.be.Number().and.aboveOrEqual(0)
 
-  assignment.state.should.match(/previous|new|unknown/)
+  assignment.state.should.match(/已经提交|尚未提交/)
+  assignment.scored.should.be.Boolean()
 }
 
 describe('Test for LearHelperUtil Class', () => {
-  /*
   describe('1. test method "getCourseList"', function () {
     // avoid timeout error
     this.timeout(0)
@@ -82,17 +83,19 @@ describe('Test for LearHelperUtil Class', () => {
         nock(outerDomain)
           .get('/MultiLanguage/lesson/student/course_info.jsp')
           .query((query) => {
-            return ('course_id' in query && query.course_id === courseIDs[index])
+            return (query.course_id === courseIDs[index])
           })
           .reply(200, response)
       })
-
+      
+      await sleep(2000)
+      
       const courses = await learnHelper.getCourseList()
-      // assertCourses(courses)
-      console.log('courses = ', courses)
+      assertCourses(courses)
+      // console.log('courses = ', courses)
     })
   })
-  /*
+  
   describe('2. test method "getDocuments"', function () {
     // avoid timeout error
     this.timeout(0)
@@ -105,7 +108,7 @@ describe('Test for LearHelperUtil Class', () => {
       nock(outerDomain)
         .get('/MultiLanguage/lesson/student/download.jsp')
         .query((query) => {
-          return ('course_id' in query && query.course_id === courseID)
+          return (query.course_id === courseID)
         })
         .reply(200, response)
 
@@ -115,7 +118,6 @@ describe('Test for LearHelperUtil Class', () => {
       // console.log('docs = ', docs)
     })
   })
-  */
   
   describe('3. test method "getAssignments"', function () {
     // avoid timeout error
@@ -127,15 +129,15 @@ describe('Test for LearHelperUtil Class', () => {
       const courseID = '137928'
 
       const assignmentIDs = ['714167', '715467', '710921', '718565', '721805', '726511', '718619',
-      '718621', '725765', '725767', '728273']
+        '718621', '725765', '725767', '728273']
 
       const recIDs = ['null', '4695237', '4695860', '4752631', '4755578', '4773263', '4881717',
-      '4732442', '4730977', '4811077', '4829496']
+        '4732442', '4730977', '4811077', '4829496']
 
       nock(outerDomain)
         .get('/MultiLanguage/lesson/student/hom_wk_brw.jsp')
         .query((query) => {
-          return ('course_id' in query && query.course_id == courseID)
+          return (query.course_id === courseID)
         })
         .reply(200, response)
       
@@ -143,33 +145,26 @@ describe('Test for LearHelperUtil Class', () => {
       assignmentIDs.forEach(async (ele, index) => {
         const responseDetail = await readFile(`${__dirname}\\test-assignment-info\\assignment-detail-${index}.html`)
         const responseView = await readFile(`${__dirname}\\test-assignment-info\\assignment-view-${index}.html`)
-
-        // console.log('responseDetail = ', responseDetail.toString())
         
-        nock('https://learn.tsinghua.edu.cn')
+        nock('http://learn.tsinghua.edu.cn')
           .get('/MultiLanguage/lesson/student/hom_wk_detail.jsp')
           .query((query) => {
-            console.log('query = ', query)
-            matchResult = 'course_id' in query && query.course_id === courseID && 'id' in query && query.id === ele && 'rec_id' in query && query.rec_id === recIDs[index]
-            console.log('matchResult = ', matchResult)
-            return ('course_id' in query && query.course_id === courseID && 'id' in query && query.id === ele && 'rec_id' in query && query.rec_id === recIDs[index])
+            return (query.course_id === courseID && query.id === ele && query.rec_id === recIDs[index])
           })
           .reply(200, responseDetail)
         
-        nock('https://learn.tsinghua.edu.cn')
+        nock('http://learn.tsinghua.edu.cn')
           .get('/MultiLanguage/lesson/student/hom_wk_view.jsp')
           .query((query) => {
-            return ('course_id' in query && query.course_id === courseID && 'id' in query && query.id === ele && 'rec_id' in query && query.rec_id === recIDs[index])
+            return (query.course_id === courseID && query.id === ele && query.rec_id === recIDs[index])
           })
           .reply(200, responseView)
       })
       
       await sleep(2000)
-      
       const assignments = await learnHelper.getAssignments(courseID)
-      // assertAssignments(assignments)
+      assertAssignments(assignments)
       // console.log('assignments = ', assignments)
     })
   })
-  
 })
