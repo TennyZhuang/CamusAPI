8fcaa17b6e41e4fe7f80b310d9d771555180dce1
Songzhou Yang <yangsongzhou@outlook.com>
2016-12-29 07:20:30 +0800

    add babel

---
+++ gulpfile.babel.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const gulp = require('gulp');
+const sourceMaps = require('gulp-sourcemaps');
+const clean = require('gulp-clean');
+const babel = require('gulp-babel');
+const mocha = require('gulp-mocha');
+const src = ['app/**/*.js'];
+const testSrc = ['test/**/*.js'];
+const srcOption = {base: './app'};
+const dest = './dist';
+
+gulp.task('default', ['clean'], () => {
+  return gulp.src(src, srcOption)
+    .pipe(sourceMaps.init())
+    .pipe(babel())
+    .pipe(sourceMaps.write('.', {includeContent: false, sourceRoot: '..'}))
+    .pipe(gulp.dest(dest));
+});
+
+gulp.task('clean', () => {
+  return gulp.src(dest, {read: false})
+    .pipe(clean());
+});
+
+gulp.task('test', () => {
+  return gulp.src(testSrc, {read: false})
+    .pipe(mocha({reporter: 'nyan'}));
+});
