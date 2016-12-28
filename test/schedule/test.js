ccfc9700eec88ed95b7a888c34bbccf299504ed0
yexithu <yexithu14@163.com>
2016-12-29 06:38:21 +0800

    Add test for schedule getter

--- test/schedule/test.js
+++ test/schedule/test.js
@@ -12,6 +12,29 @@ const iconv = require('iconv-lite')
 const validUsername = 'valid username'
 const validPassword = 'valid password'
 
+const assertSchedules = (schedules) => {
+  schedules.should.be.Array().and.should.not.be.empty()
+
+  const week1Schedules = schedules[0]
+  const properties = ['place', 'type', 'startTime', 'dueTime', 'date', 'content', 'description']
+  week1Schedules.forEach(async (item) => {
+    item.should.have.properties(properties)
+  })
+
+  const classActivity = week1Schedules[0]
+  classActivity.place.should.be.eql('六教6A018')
+  classActivity.type.should.be.eql('上课')
+  classActivity.dueTime.should.be.eql('16:55')
+  classActivity.startTime.should.be.eql('15:20')
+  classActivity.date.should.be.eql('2016-09-12')
+  classActivity.content.should.be.eql('模式识别基础')
+  classActivity.description.should.be.eql('')
+
+  const week17Schedules = schedules[16]
+  const examActivity = week17Schedules[0]
+  examActivity.type.should.be.eql('考试')
+}
+
 describe('1. test schedule parser', () => {
   let data = null
   let schedules = null
@@ -46,14 +69,17 @@ describe('3. test schedules getter', function () {
   it('3.1 curriculum info should be properly extracted with valid username and password', async () => {
     const buffer = await readFile(`${__dirname}/schedule.json`, 'utf8')
     const outerDomain = 'http://zhjw.cic.tsinghua.edu.cn'
-
+    const ticketDomain = 'https://id.tsinghua.edu.cn'
+    nock(ticketDomain)
+      .post('/thuser/authapi/login/ALL_ZHJW/0_0_0_0')
+      .reply(200, '{ \"status\": \"RESTLOGIN_OK\", \"ticket\": \"ticket\" }')
     nock(outerDomain)
       .get('/j_acegi_login.do')
       .query((query) => {
         return ('ticket' in query)
       })
       .reply(200, {})
-
+    console.log(1)
     nock(outerDomain)
       .get('/jxmh.do')
       .query((query) => {
@@ -61,8 +87,8 @@ describe('3. test schedules getter', function () {
       })
       .reply(200, buffer)
 
-    const classes = await scheduleUtil.getSchedule(validUsername, validPassword, true, 1)
-    console.log(classes)
+    const schedules = await scheduleUtil.getSchedule(validUsername, validPassword, true, 1)
+    assertSchedules(schedules)
   })
 })
 
