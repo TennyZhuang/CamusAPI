f059f53da49118639e16f0288a44285e45e57575
mzj14 <mzj14@mails.tsinghua.edu.cn>
2016-12-30 22:22:55 +0800

    add description to event test

--- test/event/test.js
+++ test/event/test.js
@@ -19,37 +19,39 @@ const assertEvents = (events) => {
 }
 
 const testEvent = () => {
-  describe('1. test method "parseEvents" ', () => {
-    it('1.1 event info should be properly extracted with the right test page', async () =>{
-      const data = await readFile(`${__dirname}/test.html`, 'utf8')
-      const $ = cheerio.load(data, { decodeEntities: false })
+  describe('Test for EventUtil Class', () => {
+    describe('1. test method "parseEvents" ', () => {
+      it('1.1 event info should be properly extracted with the right test page', async () =>{
+        const data = await readFile(`${__dirname}/test.html`, 'utf8')
+        const $ = cheerio.load(data, { decodeEntities: false })
 
-      const events = await eve.parseEvents($)
-      events.should.be.Array().and.should.not.be.empty()
-
-      assertEvents(events)
+        const events = await eve.parseEvents($)
+        events.should.be.Array().and.should.not.be.empty()
+        
+        assertEvents(events)
+      })
     })
-  })
 
-  describe('2. test function "fetch" and "getEventList"', function () {
-    // avoid timeout error
-    this.timeout(0)
-    it('2.1 curriculum info should be get', async () => {
-      const buffer = await readFile(`${__dirname}/test.html`)
-      // console.log(buffer)
-      const response = iconv.encode(buffer, 'GBK')
-      const outerDomain = 'http://zhjw.cic.tsinghua.edu.cn'
+    describe('2. test function "fetch" and "getEventList"', function () {
+      // avoid timeout error
+      this.timeout(0)
+      it('2.1 curriculum info should be get', async () => {
+        const buffer = await readFile(`${__dirname}/test.html`)
+        // console.log(buffer)
+        const response = iconv.encode(buffer, 'GBK')
+        const outerDomain = 'http://zhjw.cic.tsinghua.edu.cn'
 
-      nock(outerDomain)
-        .get('/portal3rd.do')
-        .query((query) => {
-          return ('url' in query && 'm' in query)
-        })
-        .reply(200, response)
+        nock(outerDomain)
+          .get('/portal3rd.do')
+          .query((query) => {
+            return ('url' in query && 'm' in query)
+          })
+          .reply(200, response)
 
-      await eve.fetch()
-      const events = eve.getEventList()
-      assertEvents(events)
+        await eve.fetch()
+        const events = eve.getEventList()
+        assertEvents(events)
+      })
     })
   })
 }
