1f493f19fd90343ce19c5a1be3ca5ae6a21bd9c4
mzj14 <mzj14@mails.tsinghua.edu.cn>
2016-12-30 22:38:18 +0800

    add description to schedule test

--- test/schedule/test.js
+++ test/schedule/test.js
@@ -34,61 +34,63 @@ const assertSchedules = (schedules) => {
 }
 
 const testSchedule = () => {
-  describe('1. test schedule parser', () => {
-    let data = null
-    let schedules = null
-    before(async () => {
-      data = await readFile(`${__dirname}/schedule.json`, 'utf8')
-      schedules = await scheduleUtil.parseSchedule(data)
-    })
+  describe('Test for ScheduleUtil class', () => {
+    describe('1. test schedule parser', () => {
+      let data = null
+      let schedules = null
+      before(async () => {
+        data = await readFile(`${__dirname}/schedule.json`, 'utf8')
+        schedules = await scheduleUtil.parseSchedule(data)
+      })
 
-    it('1.1 should return a schedules with right length', async () => {
-      schedules.should.be.Array().and.have.length(137)
-    })
-    it('1.2 should return a json with specific field', async () => {
-      const properties = ['place', 'type', 'startTime', 'dueTime', 'date', 'content', 'description']
-      schedules.forEach(async (item) => {
-        item.should.have.properties(properties)
+      it('1.1 should return a schedules with right length', async () => {
+        schedules.should.be.Array().and.have.length(137)
+      })
+      it('1.2 should return a json with specific field', async () => {
+        const properties = ['place', 'type', 'startTime', 'dueTime', 'date', 'content', 'description']
+        schedules.forEach(async (item) => {
+          item.should.have.properties(properties)
+        })
       })
     })
-  })
 
-  describe('2. test schedules splitter', () => {
-    it('2.1 should split schedules to 18 weeks', async() => {
-      const data = await readFile(`${__dirname}/schedule.json`, 'utf8')
-      const schedules = await scheduleUtil.parseSchedule(data)
-      const weekSchedules = await scheduleUtil.splitSemesterSchedule(schedules)
-      weekSchedules.should.be.Array().and.have.length(18)
+    describe('2. test schedules splitter', () => {
+      it('2.1 should split schedules to 18 weeks', async() => {
+        const data = await readFile(`${__dirname}/schedule.json`, 'utf8')
+        const schedules = await scheduleUtil.parseSchedule(data)
+        const weekSchedules = await scheduleUtil.splitSemesterSchedule(schedules)
+        weekSchedules.should.be.Array().and.have.length(18)
+      })
     })
-  })
 
-  describe('3. test schedules getter', function () {
-    // avoid timeout error
-    this.timeout(0)
-    it('3.1 curriculum info should be properly extracted with valid username and password', async () => {
-      const buffer = await readFile(`${__dirname}/schedule.json`, 'utf8')
-      const outerDomain = 'http://zhjw.cic.tsinghua.edu.cn'
-      const ticketDomain = 'https://id.tsinghua.edu.cn'
-      nock(ticketDomain)
-        .post('/thuser/authapi/login/ALL_ZHJW/0_0_0_0')
-        .reply(200, '{ \"status\": \"RESTLOGIN_OK\", \"ticket\": \"ticket\" }')
+    describe('3. test schedules getter', function () {
+      // avoid timeout error
+      this.timeout(0)
+      it('3.1 curriculum info should be properly extracted with valid username and password', async () => {
+        const buffer = await readFile(`${__dirname}/schedule.json`, 'utf8')
+        const outerDomain = 'http://zhjw.cic.tsinghua.edu.cn'
+        const ticketDomain = 'https://id.tsinghua.edu.cn'
+        nock(ticketDomain)
+          .post('/thuser/authapi/login/ALL_ZHJW/0_0_0_0')
+          .reply(200, '{ \"status\": \"RESTLOGIN_OK\", \"ticket\": \"ticket\" }')
 
-      nock(outerDomain)
-        .get('/j_acegi_login.do')
-        .query((query) => {
-          return ('ticket' in query)
-        })
-        .reply(200, {})
+        nock(outerDomain)
+          .get('/j_acegi_login.do')
+          .query((query) => {
+            return ('ticket' in query)
+          })
+          .reply(200, {})
 
-      nock(outerDomain)
-        .get('/jxmh.do')
-        .query((query) => {
-          return ('m' in query && 'p_start_date' in query)
-        })
-        .reply(200, buffer)
+        nock(outerDomain)
+          .get('/jxmh.do')
+          .query((query) => {
+            return ('m' in query && 'p_start_date' in query)
+          })
+          .reply(200, buffer)
 
-      const schedules = await scheduleUtil.getSchedule(validUsername, validPassword, true, 1)
-      assertSchedules(schedules)
+        const schedules = await scheduleUtil.getSchedule(validUsername, validPassword, true, 1)
+        assertSchedules(schedules)
+      })
     })
   })
 }
